510ba7fbdeb2901e536a1424344d0a8bfd319592
SevenWoos <swenwu96@gmail.com>
2026-02-13 09:45:35 -0800

    Delete products from cart when clicking delete

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,5 +1,5 @@
 // This variable can now be used outside of cart.js
-export const cart = [
+export let cart = [
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
     quantity: 2
@@ -30,4 +30,16 @@ export function addToCart(productId) {
       quantity: 1
     });
   }
+};
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    };
+  });
+
+  cart = newCart;
 };
