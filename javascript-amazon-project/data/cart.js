76d16b7cbc7b3bc243ff5853ce8de7088517deb5
SevenWoos <swenwu96@gmail.com>
2026-02-13 10:05:57 -0800

    Save cart in localStorage

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,5 +1,12 @@
 // This variable can now be used outside of cart.js
-export let cart = [
+
+// if (!cart) {
+
+// };
+
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+
+[
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
     quantity: 2
@@ -10,6 +17,10 @@ export let cart = [
     quantity: 1
   }
 ];
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+};
 
 export function addToCart(productId) {
   let matchingItem;
@@ -30,6 +41,8 @@ export function addToCart(productId) {
       quantity: 1
     });
   }
+
+  saveToStorage();
 };
 
 export function removeFromCart(productId) {
@@ -42,4 +55,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 };
