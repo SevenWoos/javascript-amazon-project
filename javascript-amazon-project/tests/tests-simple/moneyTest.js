28dfcae6c8f06c7c742c3175fd8e4b9293276fef
SevenWoos <swenwu96@gmail.com>
2026-02-26 12:34:19 -0800

    Rename tests-jasmine folder to tests.

--- javascript-amazon-project/tests/tests-simple/moneyTest.js
+++ javascript-amazon-project/tests/tests-simple/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../scripts/utils/money.js';
+import {formatCurrency} from '../../scripts/utils/money.js';
 // Testing Framework = external library that helps us write tests easier.
 
 console.log('test suite: formatCurrency');
