e80de7127012b946a4ade8646a70a22ae0348e8b
SevenWoos <swenwu96@gmail.com>
2026-02-25 22:11:55 -0800

    Create automated tests for addToCart.

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,30 +1,35 @@
-// This variable can now be used outside of cart.js
+export let cart;
 
-// if (!cart) {
+loadFromStorage();
 
-// };
+export function loadFromStorage() {
+  // This variable can now be used outside of cart.js
 
-export let cart = JSON.parse(localStorage.getItem('cart')) ||
+  // if (!cart) {
+  
+  // };
+  cart = JSON.parse(localStorage.getItem('cart')) ||
 
-[
-  {
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
-    quantity: 2, 
-    deliveryOptionId: '1'
-  }, 
+  [
+    {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
+      quantity: 2, 
+      deliveryOptionId: '1'
+    }, 
 
-  {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', 
-    quantity: 1, 
-    deliveryOptionId: '2'
-  }
-];
+    {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', 
+      quantity: 1, 
+      deliveryOptionId: '2'
+    }
+  ];
+};
 
 function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
 };
 
-export function addToCart(productId) {
+export function addToCart(productId, quantity) {
   let matchingItem;
 
   // Check for duplicate items do we can increase quantity
@@ -34,10 +39,10 @@ export function addToCart(productId) {
     }
   });
 
-  const quantitySelector = document.querySelector(
-      `.js-quantity-selector-${productId}`
-  );
-  const quantity = Number(quantitySelector.value);
+  // const quantitySelector = document.querySelector(
+  //     `.js-quantity-selector-${productId}`
+  // );
+  // const quantity = Number(quantitySelector.value);
 
   if(matchingItem) {
     matchingItem.quantity+=quantity;
