a12c60de2a128ee85451a07dc92e679a9eb19c57
SevenWoos <swenwu96@gmail.com>
2026-03-09 22:34:56 -0700

    Load products from the backend.

--- javascript-amazon-project/data/products.js
+++ javascript-amazon-project/data/products.js
@@ -96,6 +96,36 @@ const object3 = {
 object3.method();
 */
 
+// Load products using backend.
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    // Convert JSON to JavaScript array and then convert from objects to classes. 
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      // Convert to Clothing class
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      };
+    
+      // Convert each product object into a product class.
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    // Wait for HTTP request to finish and then run callback function to render products.
+    // Callback = function to run in the future.
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+};
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -764,3 +794,4 @@ export const products = [
   // Convert each product object into a product class.
   return new Product(productDetails);
 });
+*/
